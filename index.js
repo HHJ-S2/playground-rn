18a0a7ba43793e201eaf4fc55bfb2b24dedf4307
HHJ-S2 <berdle0122@gmail.com>
2024-07-31 20:06:54 +0900

    fix: lint error 수정

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './App.tsx';
+import { name as appName } from './app.json';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 AppRegistry.registerComponent(appName, () => App);
